82aba9f43904194de3b9db108659204d77d8c711
Danila Catalin-Gabriel <danila.catalin.gabriel55@gmail.com>
2021-01-10 19:01:38 +0200

    pre-update angular

---
+++ PillsReminder/PillsReminder/Models/DozaMedicamentRequestId.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PillsReminder.Models
+{
+    public class DozaMedicamentRequestId
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ PillsReminder/PillsReminder/Models/DozaMedicamentRequestIdLuata.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PillsReminder.Models
+{
+    public class DozaMedicamentRequestIdLuata
+    {
+        public int Id { get; set; }
+        public bool Luata { get; set; }
+    }
+}
